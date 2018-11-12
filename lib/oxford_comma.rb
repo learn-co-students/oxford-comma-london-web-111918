7061bcf396a3f636c3e8c6871dc37d304757b6d1
Kevin Sutton <kevmsutton@gmail.com>
2018-11-12 18:06:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
-
+def oxford_comma(elements)
+	if elements.length == 1
+	return elements.shift
+  elsif elements.length == 2
+  return "#{elements[0]} and #{elements[1]}"
+  elsif elements.length > 2
+  last = elements.pop
+  first_elements = elements.join (", ")
+  return "#{first_elements}, and #{last}"
 end
+end
+
+oxford_comma(["fiddleheads"])

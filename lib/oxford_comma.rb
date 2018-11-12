4bf4b79d558107e7c3528e7016b4918466564bbd
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-11-12 14:32:00 +0000

    Comma chameleon

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comma(a)
+  if a.size == 1
+  a.join
+  elsif a.size == 2
+  a.join(" and ")
+  else  x = a.pop()
+        y = a.join(", ")
+        y +=  ", and #{x}"
+  end
 end
